a25780ce1ac16e2bfa40dcaebdc55279a83056ed
mariamalaidi <mariam.alaide@gmail.com>
2021-01-31 01:54:10 +0200

    Adding Time, Frames for the Game

--- src/main/java/ca/world/game/Game.java
+++ src/main/java/ca/world/game/Game.java
@@ -2,6 +2,9 @@ package ca.world.game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 public class Game extends Canvas implements Runnable{
     public static final int WIDTH = 160; // width of the map
@@ -10,6 +13,9 @@ public class Game extends Canvas implements Runnable{
     public static final String NAME = "WORLD NAVIGATOR";
     private JFrame frame;
     private boolean running = false;
+    private int tickCount = 0;
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 
     public Game(){
         setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT * SCALE));
@@ -35,12 +41,58 @@ public class Game extends Canvas implements Runnable{
     }
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D/60D;
+        int ticks = 0;
+        int frames = 0 ;
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0 ;
         while(running){
-            System.out.println("Hello world");
+            long now = System.nanoTime();
+            delta+=(now - lastTime)/nsPerTick;
+            lastTime = now;
+            boolean shouldRender = false;
+            while(delta >= 1){
+                ticks++;
+                tick();
+                delta-=1;
+                shouldRender = true;
+            }
+            if(shouldRender) {
+                frames++;
+                render();
+            }
+            if(System.currentTimeMillis() - lastTimer >= 1000){
+                lastTimer += 1000;
+                System.out.println(frames+" , "+ticks);
+                frames = 0 ;
+                ticks = 0 ;
+            }
         }
     }
 
+    public void tick(){
+        tickCount++;
+        for(int i = 0 ; i < pixels.length ; i++){
+            pixels[i] = i+tickCount;
+        }
+    }
+
+    public void render(){
+        BufferStrategy bufferStrategy = getBufferStrategy();
+        if(bufferStrategy == null){
+            createBufferStrategy(3);
+            return ;
+        }
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0,getWidth(), getHeight());
+        graphics.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        graphics.dispose();
+        bufferStrategy.show();
+    }
     public static void main(String[] args) {
         new Game().start();
     }
 }
+
